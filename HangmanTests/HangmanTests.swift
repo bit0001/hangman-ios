22365de83e1f1176497906fefa4bc79002ed3625
M <lmiguelvargasf@gmail.com>
2016-11-11 15:44:15 -0500

    test: Test basic functionality of guessLetter

--- HangmanTests/HangmanTests.swift
+++ HangmanTests/HangmanTests.swift
@@ -19,4 +19,28 @@ class HangmanTests: XCTestCase {
         super.tearDown()
     }
     
+    func testMaskedWordGuessedLettersAndTriesAreUpdatedProperlyCase0() {
+        let game = HangmanGame(word: "hello", tries: 7)
+        var set = Set<Character>()
+        var letter: Character = "h"
+        set.insert(letter)
+        
+        game.guessLetter(letter: letter)
+        XCTAssertEqual(game.guessedWord, "h????")
+        XCTAssertEqual(game.guessedLetters, set)
+        XCTAssertEqual(game.tries, 7)
+    }
+    
+    func testMaskedWordAndGuessedLettersAreUpdatedProperlyCase1() {
+        let game = HangmanGame(word: "computer", tries: 7)
+        var set = Set<Character>()
+        let letter: Character = "x"
+        set.insert(letter)
+        
+        game.guessLetter(letter: letter)
+        XCTAssertEqual(game.guessedWord, "????????")
+        XCTAssertEqual(game.guessedLetters, set)
+        XCTAssertEqual(game.tries, 6)
+    }
+    
 }
