f010e6a16b9dad0f6e677ab5269066dab84f03ea
M <lmiguelvargasf@gmail.com>
2016-11-11 15:01:55 -0500

    model: Add parameters
    Add guessedWord and guessedLetters to HangmanGame model.

--- Hangman/HangmanGame.swift
+++ Hangman/HangmanGame.swift
@@ -3,9 +3,14 @@ import Foundation
 class HangmanGame {
     let word: String
     let tries: Int
+    var guessedWord: String
+    var guessedLetters: Set<Character>
     
     init(word: String, tries: Int) {
         self.word = word
         self.tries = tries
+        guessedWord = String(repeating: "?", count: word.characters.count)
+        guessedLetters = Set<Character>()
     }
+    
 }
