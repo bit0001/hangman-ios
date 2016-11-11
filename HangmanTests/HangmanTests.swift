8bbaba88c9c85a21808e0da58d8b8a1ef3f2500b
M <lmiguelvargasf@gmail.com>
2016-11-11 16:44:44 -0500

    test: Test winning and losing cases

--- HangmanTests/HangmanTests.swift
+++ HangmanTests/HangmanTests.swift
@@ -39,6 +39,118 @@ class HangmanTests: XCTestCase {
         assertGame(game: game, expectedString: "???", expectedSet: set, expectedTries: 2)
     }
     
+    func testWinningCase0() {
+        var set = Set<Character>()
+        var letter: Character = "h"
+        
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "h??", expectedSet: set, expectedTries: 3)
+
+        letter = "m"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "h?m", expectedSet: set, expectedTries: 3)
+
+        letter = "i"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "him", expectedSet: set, expectedTries: 3)
+    }
+    
+    func testWinningCase1() {
+        var set = Set<Character>()
+        var letter: Character = "h"
+        
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "h??", expectedSet: set, expectedTries: 3)
+        
+        
+        letter = "x"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "h??", expectedSet: set, expectedTries: 2)
+        
+        letter = "m"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "h?m", expectedSet: set, expectedTries: 2)
+        
+        letter = "m"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "h?m", expectedSet: set, expectedTries: 2)
+        
+        letter = "x"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "h?m", expectedSet: set, expectedTries: 2)
+        
+        letter = "y"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "h?m", expectedSet: set, expectedTries: 1)
+        
+        letter = "z"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "h?m", expectedSet: set, expectedTries: 0)
+        
+        letter = "i"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "him", expectedSet: set, expectedTries: 0)
+    }
+    
+    func testLosingCase0() {
+        var set = Set<Character>()
+        var letter: Character = "x"
+        set.insert(letter)
+    
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "???", expectedSet: set, expectedTries: 2)
+        
+        letter = "y"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "???", expectedSet: set, expectedTries: 1)
+        
+        letter = "z"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "???", expectedSet: set, expectedTries: 0)
+    }
+    
+    func testLosingCase1() {
+        var set = Set<Character>()
+        var letter: Character = "h"
+        set.insert(letter)
+        
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "h??", expectedSet: set, expectedTries: 3)
+        
+        letter = "i"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "hi?", expectedSet: set, expectedTries: 3)
+        
+        letter = "y"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "hi?", expectedSet: set, expectedTries: 2)
+        
+        letter = "z"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "hi?", expectedSet: set, expectedTries: 1)
+        
+        letter = "x"
+        set.insert(letter)
+        game.guessLetter(letter: letter)
+        assertGame(game: game, expectedString: "hi?", expectedSet: set, expectedTries: 0)
+    }
+    
     private func assertGame(game: HangmanGame, expectedString: String, expectedSet: Set<Character>, expectedTries: Int) {
         XCTAssertEqual(game.guessedWord, expectedString)
         XCTAssertEqual(game.guessedLetters, expectedSet)
