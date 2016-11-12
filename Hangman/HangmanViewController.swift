c039cbe8a6c7f23b78b4c9aceba529158ea86d81
M <lmiguelvargasf@gmail.com>
2016-11-11 20:03:09 -0500

    refactor: Remove comments and method

--- Hangman/HangmanViewController.swift
+++ Hangman/HangmanViewController.swift
@@ -16,12 +16,6 @@ class HangmanViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func guess() {
